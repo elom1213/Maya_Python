afeea927954db8229c0d1afbecc3d8263d78e965
JunnyPark <junny.park1213@gmail.com>
2026-05-11 21:24:18 +0900

    update importinf config method

--- JUN_All/tools/A00000_base/base.py
+++ JUN_All/tools/A00000_base/base.py
@@ -9,7 +9,7 @@ import maya.cmds as cmds;
 import maya.mel as mel
 from functools import partial
 
-import config
+from . import config
 from Framework.ui import JUN_mod_tsl, JUN_mod_radCol, JUN_mod_colorThem
 
 class JUN_ToolUI_base:
