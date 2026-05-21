1da1045e74c9358fcc3280adf53ff1eb12b42d2c
JunnyPark <junny.park1213@gmail.com>
2026-05-21 22:29:13 +0900

    merge Dnable to master. update  jointTool v02.01

--- JUN_All/dev/remove_pycache_for_dev.py
+++ JUN_All/dev/remove_pycache_for_dev.py
@@ -2,5 +2,5 @@ import remove_pycache as rmv_pyc
 import os
 import shutil
 
-DEV_ROOT = r"C:\Users\USER\Desktop\JP\0030_maya_python_JUN\Maya_Python"
+DEV_ROOT = r"G:/D_link_dir/02_Maya_python_Jun/JUN_All"
 rmv_pyc.remove_pycache(DEV_ROOT)
